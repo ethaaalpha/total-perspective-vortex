f1559182e8cb401adf5b3313cc986e66fdce1df6
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-02-24 16:45:59 +0100

    fourier anylisys is cool

--- src/preprocessing/signal.py
+++ src/preprocessing/signal.py
@@ -1,19 +1,46 @@
 from abc import ABC, abstractmethod
 from mne.io import Raw
+from matplotlib import pyplot as pp
+import numpy as np
 
 class AbstractSignalAnalyser(ABC):
-    def analyse(raw: Raw):
+    def __init__(self, raw: Raw):
+        self.raw = raw
+        self.data = np.array(raw.get_data())
+        # number of points per seconds (Hz)
+        self.sampling_frequency = raw.info["sfreq"]
+
+    @abstractmethod
+    def analyse(self):
         """Analyse the passed raw mne data"""
         pass
 
-    @abstractmethod
-    def extract_frequencies(signal):
-        """This function is supposed to return the list of frequencies that are inside the passed signal"""
-        pass
 
 class DiscreteFourierTransform(AbstractSignalAnalyser):
     def analyse(raw):
         return super().analyse()
-    
-    def extract_frequencies(signal):
-        return super().extract_frequencies()
+
+
+class FastFourierTransform(AbstractSignalAnalyser):
+
+    def __init__(self, raw, name="FastFourierTransform"):
+        self.name = name
+        super().__init__(raw)
+
+    def analyse(self):
+        pp.figure()
+        pp.xlabel("Frequency (Hz)")
+        pp.ylabel("Magnitude")
+        pp.xlim(0, 40)
+        pp.title(self.name)
+        pp.gcf().canvas.manager.set_window_title("Signal Analysis")
+        pp.grid()
+
+        for signal in self.data:
+            N = len(signal)
+            fourier_transform = np.fft.fft(signal)
+            fourier_frequencies = np.fft.fftfreq(N, 1 / self.sampling_frequency)
+
+            # we use abs to compute the imaginary part and real part (cf: magnitude of complex numbers)
+            # we do :N // 2 to only keep positive frequencies due to complex values symmetry
+            pp.plot(fourier_frequencies[:N // 2], np.abs(fourier_transform[:N // 2]))
--- src/train.py
+++ src/train.py
@@ -1,5 +1,6 @@
 from mne.io import read_raw_edf
 from matplotlib import pyplot as pp
+from preprocessing.signal import FastFourierTransform
 import numpy as np
 import os 
 
@@ -14,39 +15,11 @@ def get_all_edf(parent_path: str) -> list:
             edf_files.extend(get_all_edf(path))
     return edf_files
 
-# load_file = [read_raw_edf(x) for x in get_all_edf("dataset/")]
+person = str(69).zfill(3)
+raws = [read_raw_edf(f"dataset/S{person}/S{person}R{i:02d}.edf", preload=True) for i in range(1, 15)]
 
-raw = read_raw_edf("dataset/S012/S012R02.edf", preload=True)
-raw_data = np.array(raw.get_data())
-
-sampling_frequency = raw.info["sfreq"] # number of points per seconds (Hz)
-
-def compute_fft(signal):
-    return np.fft.fft(signal)
-
-def compute_frequency(N):
-    return np.fft.fftfreq(N, d=1/sampling_frequency)
-
-for index, signal in enumerate(raw_data):
-    # print(f"index {index}")
-    N = len(signal) # number of samples
-    # k = np.arange(N) # frequencies indices (x axis)
-    # n = np.arange(N).reshape(-1, 1) # time indices (y axis)
-
-    # W = np.exp(-2j * np.pi * k * n / N)
-    # fourier_transform = np.dot(W, signal)
-    fourier_transform = compute_fft(signal)
-    frequencies = compute_frequency(N)
-
-    pp.plot(frequencies[:N // 2], np.abs(fourier_transform[:N // 2]), label=f"Channel {index}")
-    # frequencies = np.array([k * sampling_frequency / N for k in range(N)])
-
-    # pp.plot(frequencies[:N // 2], np.abs(fourier_transform[:N // 2]))
-
-pp.xlabel("Frequency (Hz)")
-pp.ylabel("Magnitude")
-pp.grid()
-pp.xlim(0, 40)
+for i, raw in enumerate(raws):
+    FastFourierTransform(raw, i).analyse()
 
 pp.show()
 
@@ -56,3 +29,26 @@ pp.show()
 # raw.plot(n_channels=2, scalings={"eeg":"3e-4"}, title="non filter", show=True)
 # raw.filter(1, 30)
 # raw.plot(n_channels=2, scalings={"eeg":"3e-4"}, title="filtered", show=True, block=True)
+
+# def compute_fft(signal):
+#     return np.fft.fft(signal)
+
+# def compute_frequency(N):
+#     return np.fft.fftfreq(N, d=1/sampling_frequency)
+
+# for index, signal in enumerate(raw_data):
+#     # print(f"index {index}")
+#     N = len(signal) # number of samples
+#     # k = np.arange(N) # frequencies indices (x axis)
+#     # n = np.arange(N).reshape(-1, 1) # time indices (y axis)
+
+#     # W = np.exp(-2j * np.pi * k * n / N)
+#     # fourier_transform = np.dot(W, signal)
+#     fourier_transform = compute_fft(signal)
+#     frequencies = compute_frequency(N)
+
+#     pp.plot(frequencies[:N // 2], np.abs(fourier_transform[:N // 2]), label=f"Channel {index}")
+#     # frequencies = np.array([k * sampling_frequency / N for k in range(N)])
+
+#     # pp.plot(frequencies[:N // 2], np.abs(fourier_transform[:N // 2]))
+
