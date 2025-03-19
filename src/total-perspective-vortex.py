d57b5e9ca78a88989e720f0c600322a602227993
ethaaalpha <geekofgamer@gmail.com>
2025-03-19 22:24:55 +0100

    typo

--- src/total-perspective-vortex.py
+++ src/total-perspective-vortex.py
@@ -3,11 +3,11 @@ from argparse import ArgumentParser
 
 dataset_arg = "The dataset folder which contains every subjects (or the needed ones). The expected structure is dataset/S00X/S00XR0Y.edf with X as the subject number and Y the task number."
 subject_arg = "The subject to use represented by X."
-experience_arg = "The experience possibilies are a combination of multiples tasks Y. "
-"T1=[3, 7, 11](open and close left or right fist), "
-"T2=[4, 8, 12](imagine opening and closing left or right fist), "
-"T3=[5, 9, 13](open and close both fists or both feet), "
-"T4=[6, 10, 14](imagine opening and closing both fists or both feet)"
+experience_arg = """The experience possibilies are a combination of multiples tasks Y.
+T1=[3, 7, 11](open and close left or right fist),
+T2=[4, 8, 12](imagine opening and closing left or right fist),
+T3=[5, 9, 13](open and close both fists or both feet),
+T4=[6, 10, 14](imagine opening and closing both fists or both feet)"""
 experience_choices = [1, 2, 3 ,4]
 task_arg = "The task Y to visualize."
 only_arg = "fourier: result of fourier transform, standard: the raw visualization of the dataset, filter: before and after filtering."
