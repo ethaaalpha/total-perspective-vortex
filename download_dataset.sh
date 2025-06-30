190e3246d75b8061763cf1d6acd5530a3ddb0b77
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-06-30 11:00:04 +0200

    fixes

--- download_dataset.sh
+++ download_dataset.sh
@@ -5,7 +5,7 @@ image_docker=physionet
 mne_folder=dataset/MNE-eegbci-data/files/eegmmidb/1.0.0
 
 docker build -t ${image_docker} - <<EOF
-FROM alpine:latest
+FROM alpine:3.22
 
 RUN adduser -D -u $(id -u) -g $(id -g) user
 RUN apk add --no-cache aws-cli
@@ -17,5 +17,5 @@ CMD ["aws", "s3", "sync", "--no-sign-request", "s3://physionet-open/eegmmidb/1.0
 EOF
 
 mkdir -p ${mne_folder}
-docker run --rm --user $(id -u):$(id -g) --mount type=bind,src=$(pwd)/${mne_folder},target=/home/user/mount/ physionet
+docker run --rm --user $(id -u):$(id -g) --mount type=bind,src=$(pwd)/${mne_folder},target=/home/user/mount/ ${image_docker}
 docker image rm ${image_docker}
